06225e84aea299b1d094827973429799b52bc65b
vinothvk <vinothvk@gmail.com>
2019-06-26 12:57:45 -0700

    Update index.spec.js

--- examples/simple/index.spec.js
+++ examples/simple/index.spec.js
@@ -1,7 +1,10 @@
 /* eslint-env mocha */
 
+console.log('running');
 describe('main', function () {
+    console.log('inside describe');
     describe('#endsWith()', function () {
+        console.log('inside endswith');
         it('should return true when the value ends with the suffix', function () {
             assert.equal(true, endsWith("abcd", "cd"));
         });
