2594218926a708ab091b9ca4044aa23f86fe0083
vinothvk <vinothvk@gmail.com>
2019-06-26 12:56:41 -0700

    Update index.spec.js

--- examples/simple/index.spec.js
+++ examples/simple/index.spec.js
@@ -1,8 +1,13 @@
 /* eslint-env mocha */
-const expect = require('chai').expect;
 
-describe('ChromeLauncher', function () {
-  it('works', function () {
-    expect(1 + 1).to.be.eql(2);
-  });
+describe('main', function () {
+    describe('#endsWith()', function () {
+        it('should return true when the value ends with the suffix', function () {
+            assert.equal(true, endsWith("abcd", "cd"));
+        });
+
+        it('should return false when the value does not end with the suffix', function () {
+            assert.equal(false, endsWith("abcd", "cde"));
+        });
+    });
 });
