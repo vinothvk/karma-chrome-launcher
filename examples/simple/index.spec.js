7bc2a79420becb403da887d01881576e9c8a28a4
vinothvk <vinothvk@gmail.com>
2019-06-26 12:01:14 -0700

    Update index.spec.js

--- examples/simple/index.spec.js
+++ examples/simple/index.spec.js
@@ -1,7 +1,8 @@
 /* eslint-env mocha */
+const expect = require('chai').expect;
 
 describe('ChromeLauncher', function () {
   it('works', function () {
-    return 1 + 1 === 2
-  })
-})
+    expect(1 + 1).to.be.eql(2);
+  });
+});
